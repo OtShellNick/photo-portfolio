b6fec7b2a02325e64f2e2a7f1beb9d887995de0d
Aleksandr <exte92@gmail.com>
2023-06-26 16:28:22 +0300

    add animation

--- src/components/Preview/Preview.jsx
+++ src/components/Preview/Preview.jsx
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import './Preview.scss';
 
-const Preview = ({ ref }) => {
+const Preview = () => {
+    const first = useRef(null);
+    const second = useRef(null);
+    const third = useRef(null);
+
+    useEffect(() => {
+        if (first) first.current.style.transform = 'translateX(0)';
+        if (second) setTimeout(() => second.current.style.transform = 'translateX(0)', 1000);
+        if (third) setTimeout(() => third.current.style.transform = 'translateX(0)', 2000);
+    }, [])
 
     return <div className='preview'>
         <div className='container preview__content'>
             <div>
-                <p className='preview__content_text-first'>Красивые фото.</p>
-                <p className='preview__content_text-second'>Настоящие эмоции.</p>
-                <p className='preview__content_text-third'>Сейчас.</p>
+                <p ref={first} className='preview__content_text-first'>Красивые фото.</p>
+                <p ref={second} className='preview__content_text-second'>Настоящие эмоции.</p>
+                <p ref={third} className='preview__content_text-third'>Сейчас.</p>
             </div>
         </div>
     </div>
